4f79c58e5d4e8dd7f31f5a81fea9d3840074d875
darren-a <darren_adkinson@hotmail.com>
2015-12-31 13:20:11 -0800

    first cut at test extension

--- test.js
+++ test.js
@@ -1,2 +1,27 @@
-some script here
-and some more...
+// 12/31/15
+// darren-a
+// this is boilerplate code copied from 
+// https://github.com/LLK/scratchx/wiki#user-content-setting-up-crossdomainxml
+
+
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('darren-a-test-extension', descriptor, ext);
+})({});
+
+
